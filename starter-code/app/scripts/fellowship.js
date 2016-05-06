50d5c77edf08bcc5c32962a41db626f95424bdb6
SBRKS <sbrks3@gmail.com>
2016-05-05 23:57:27 -0700

    work through part 6

--- starter-code/app/scripts/fellowship.js
+++ starter-code/app/scripts/fellowship.js
@@ -25,9 +25,18 @@ var body = document.querySelector('body');
 
 function makeMiddleEarth() {
   // create a section tag with an id of middle-earth
+    var midEarth = document.createElement("section"); 
+        "section".idName="middle-earth";
+    for (var i=0; i < lands.length; i++) {
+  var land = document.createElement('article');
+
   // inside, add each land as an article tag
+    land.innerHTML = '<h1>' + lands[i] + '</h1>';
+    midEarth.appendChild(land);
+  }
   // inside each article tag include an h1 with the name of the land
-  // append middle-earth to your document body
+  body.appendChild(midEarth);
+
 }
 
 makeMiddleEarth();
@@ -35,19 +44,52 @@ makeMiddleEarth();
 
 // Part 2
 
+
+var theShire = body.querySelectorAll('article')[0];
+
 function makeHobbits() {
   // display an unordered list of hobbits in the shire (which is the second article tag on the page)
+  var hobbitList = document.createElement('ul');
+  for (var i=0; i <hobbits.length; i++) {
+    //give each hobbit a class of hobbit
+    hobbit.className = 'hobbit';
+    hobbit.innerText = hobbits[i];
+    hobbitList.appendChild(hobbit);
   // give each hobbit a class of hobbit
 }
+  theShire.appendChild(hobbitList);
+}
+makeHobbits();
+
+
+function makeHobbits() {
+  // display an unordered list of hobbits in the shire (which is the first article tag on the page)
+  var hobbitList = document.createElement('ul');
+  for(var i = 0, len = hobbits.length; i < len; i++){
+  // give each hobbit a class of hobbit
+    var hobbit = document.createElement('li');
+    hobbit.className = 'hobbit';
+    hobbit.innerText = hobbits[i];
+    hobbitList.appendChild(hobbit);
+  }
+  theShire.appendChild(hobbitList);
+}
+
+
 
 
 // Part 3
 
 function keepItSecretKeepItSafe() {
   // create a div with an id of 'the-ring'
+    var theRing = document.createElement('div');
+    theRing.setAttribute('id', 'the-ring');
   // give the div a class of 'magic-imbued-jewelry'
+    theRing.setAttribute ('class', 'magic-imbued-jewelry');
   // add an event listener so that when a user clicks on the ring, the nazgulScreech function (provided) is invoked
+  theRing.addEventListener('click', nazgulScreech);
   // add the ring as a child of Frodo
+  frodo.appendChild(thering);
 }
 
 
@@ -56,31 +98,59 @@ function keepItSecretKeepItSafe() {
 
 function makeBuddies() {
   // create an aside tag
+ var aside = document.createElement('aside');
   // attach an unordered list of the 'buddies' in the aside
-  // insert your aside as a child element of rivendell
-}
+  //  // display an unordered list of hobbits in the shire (which is the second article tag on the page)
+        //var hobbitList = document.createElement('ul');
+        //for (var i=0; i <hobbits.length; i++) {
+    var buddyList = document.createElement('ul');
+    for (var i=0, len = buddies.length; i < len; i++) {
+      var buddy = document.createElement('li');
+      buddy.textContent = buddies[i];
+      buddyList.appendChild(buddy);
+    }
 
+  // insert your aside as a child element of rivendell
+  aside.appendChild(buddyList);
+  rivendell.appendChild(aside);
+}
+makeBuddies();
 
 // Part 5
 
-
+var strider = rivendell.querySelectorAll('li')[3];
 function beautifulStranger() {
   // change the 'Strider' textnode to 'Aragorn'
+  strider.textContent = 'Aragon';
 }
+
+beautifulStranger();
 
 
 // Part 6
 
+var hobbits = theShire.querySelector('ul');
+
 function leaveTheShire() {
   // assemble the hobbits and move them to Rivendell
+
+   rivendell.appendChild(hobbits);
 }
 
+leaveTheShire();
 
 // Part 7
 
 
 function forgeTheFellowShip() {
+
+
   // create a new div called 'the-fellowship' within rivendell
+
+      var theFellowship = rivendell.createElement("div"); 
+      theFellowship.idName = "the-fellowship";
+
+
   // add each hobbit and buddy one at a time to 'the-fellowship'
   // after each character is added make an alert that they have joined your party
 }
